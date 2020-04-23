23f09260a3d029d6bc80093dd8e5001a0b2da516
Robert Gruner <robert.gruner@jambit.com>
2020-04-23 16:50:58 +0200

    Notiz in Datei schreiben & lesen

---
+++ app/src/main/java/com/example/mynote/NoteDetailsActivity.kt
@@ -0,0 +1,33 @@
+package com.example.mynote
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.EditText
+
+class NoteDetailsActivity : AppCompatActivity() {
+    private lateinit var editText: EditText
+    private lateinit var noteDetailsActivityListener: NoteDetailsActivityListener
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_details)
+
+        editText = findViewById(R.id.noteInput)
+        noteDetailsActivityListener = NoteDetailsActivityListener(this, editText)
+        editText.setText(noteDetailsActivityListener.readNoteContent())
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_note_details, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // TODO : Use super für back button
+        Log.d(NoteDetailsActivity::class.java.simpleName, item.itemId.toString())
+        return noteDetailsActivityListener.onMenuItemClick(item)
+    }
+}
---
+++ app/src/main/java/com/example/mynote/NoteDetailsActivityListener.kt
@@ -0,0 +1,42 @@
+package com.example.mynote
+
+import android.content.Context
+import android.view.MenuItem
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import java.io.File
+
+class NoteDetailsActivityListener(
+    val activity: AppCompatActivity,
+    val textInput: EditText
+) : MenuItem.OnMenuItemClickListener {
+    private var fileName: String = activity.getString(R.string.noteFileName)
+
+    override fun onMenuItemClick(item: MenuItem?): Boolean {
+        when(item?.itemId) {
+            R.id.saveNote -> saveNoteContent()
+            R.id.deleteNote -> deleteNoteContent()
+        }
+        return true
+    }
+
+    fun readNoteContent(): String {
+        val file = File(activity.filesDir, fileName)
+        return if (file.exists()) {
+            val readStream = activity.openFileInput(fileName).bufferedReader(Charsets.UTF_8)
+            val content = readStream.readText()
+            readStream.close()
+            return content
+        } else ""
+    }
+
+    private fun saveNoteContent() {
+        activity.openFileOutput(fileName, Context.MODE_PRIVATE).use {
+            it?.write(textInput.text.toString().toByteArray(Charsets.UTF_8))
+        }
+    }
+
+    private fun deleteNoteContent() {
+        TODO("Not yet implemented")
+    }
+}
