871e3d65bdb39be91348bb7923c6c419a1135ec4
Robert Gruner <robert.gruner@jambit.com>
2020-04-23 10:54:55 +0200

    Button löst DIAL action aus

--- app/src/main/java/com/example/mynote/SettingsActivity.kt
+++ app/src/main/java/com/example/mynote/SettingsActivity.kt
@@ -1,12 +1,25 @@
 package com.example.mynote
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+    }
+
+    fun clickHandler(v: View) {
+        when (v.id) {
+            R.id.btnCallHotline -> {
+                val hotlineNumber = getString(R.string.hotlineNumber)
+                val intent = Intent(Intent.ACTION_DIAL, Uri.parse(hotlineNumber))
+                startActivity(intent)
+            }
+        }
     }
 }
