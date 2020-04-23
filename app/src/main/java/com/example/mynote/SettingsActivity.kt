88904ae27f8393dc9b24671ce1154ed64872f6bc
Robert Gruner <robert.gruner@jambit.com>
2020-04-23 11:16:48 +0200

    Direkten CALL anstatt DIAL nutzen

--- app/src/main/java/com/example/mynote/SettingsActivity.kt
+++ app/src/main/java/com/example/mynote/SettingsActivity.kt
@@ -1,10 +1,13 @@
 package com.example.mynote
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.app.ActivityCompat
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -15,11 +18,30 @@ class SettingsActivity : AppCompatActivity() {
 
     fun clickHandler(v: View) {
         when (v.id) {
-            R.id.btnCallHotline -> {
-                val hotlineNumber = getString(R.string.hotlineNumber)
-                val intent = Intent(Intent.ACTION_DIAL, Uri.parse(hotlineNumber))
-                startActivity(intent)
-            }
+            R.id.btnCallHotline -> callHotline()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+       if (requestCode == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+           callHotline()
+       }
+    }
+
+    private fun callHotline() {
+        val hotlineNumber = getString(R.string.hotlineNumber)
+        val intent = Intent(Intent.ACTION_CALL, Uri.parse(hotlineNumber))
+        if (ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED
+        ) {
+            val permissions = arrayOf(Manifest.permission.CALL_PHONE)
+            requestPermissions(permissions, 1)
+        } else {
+            startActivity(intent)
         }
     }
 }
